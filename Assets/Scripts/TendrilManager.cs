aa34a75469b5ae061950897fe751747b8e80b53a
Thomas Chernaik <thomas.chernaik@gmail.com>
2022-06-11 18:26:42 +0100

    written the compute shader for a lil striaght line

--- Assets/Scripts/TendrilManager.cs
+++ Assets/Scripts/TendrilManager.cs
@@ -8,16 +8,27 @@ public class TendrilManager : MonoBehaviour
     public int numTendrils;
     MeshFilter meshFilter;
     int kernelHandle;
+    
     ComputeBuffer verticesBuffer;
     ComputeBuffer trianglesBuffer;
+    static readonly int vertID = Shader.PropertyToID("vertices");
+    static readonly int triID = Shader.PropertyToID("triangles");
 
 
     // Start is called before the first frame update
     void Start()
     {
         verticesBuffer = new ComputeBuffer(30, sizeof(float) * 3);
+        trianglesBuffer = new ComputeBuffer(30, sizeof(float) * 3);
+        shader.SetBuffer(kernelHandle, vertID, verticesBuffer);
+        shader.SetBuffer(kernelHandle, triID, trianglesBuffer);
 
         meshFilter = GetComponent<MeshFilter>();
+    }
+    void updateMesh()
+    {
+        var mesh = new Mesh();
+
     }
 
     // Update is called once per frame
