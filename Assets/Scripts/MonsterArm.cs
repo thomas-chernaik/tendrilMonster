8989d31816145280e71f77a473dcee57b8699692
Thomas Chernaik <thomas.chernaik@gmail.com>
2022-06-13 17:27:18 +0100

    made the arm able to hit things

--- Assets/Scripts/MonsterArm.cs
+++ Assets/Scripts/MonsterArm.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class MonsterArm : MonoBehaviour
 {
     public float intervalBetweenAttacks;
-    public float attacksPerGo;
+    public float attackNumber;
+    public float attackTime;
+    public float stationaryTime;
+    public float moveBackTime;
     //the amount to change the velocity of the sway each frame
     public float swayAmount;
     public float radiusChangeAmount;
@@ -24,6 +27,11 @@ public class MonsterArm : MonoBehaviour
     //the coords of the arm relative to the centre
     Vector3 armPosition;
     public float num;
+    bool movingBack;
+    bool stationary;
+    public bool attacking;
+
+    float timer;
 
    
     // Start is called before the first frame update
@@ -68,12 +76,64 @@ public class MonsterArm : MonoBehaviour
         armPosition *= swayRadius;
         transform.position = centre.position + armPosition;
     }
+    void Attack()
+    {
+        //if its stationary
+        if(stationary)
+        {
+            //check if stationary time is up
+            if(timer > stationaryTime)
+            {
+                stationary = false;
+                movingBack = true;
+                timer = 0;
+            }
+        }
+        //if its moving baxk
+        else if(movingBack)
+        {
+            //move it back to the original place it was
+            transform.position = Vector3.Lerp(target.position, centre.position + armPosition, timer / moveBackTime);
+            //if its done then we are done with the attack and can go back to swaying
+            if(timer > moveBackTime)
+            {
+                movingBack = false;
+                stationary = false;
+                attacking = false;
+                swaying = true;
+                timer = 0;
+            }
+        }
+        //if the arm is moving towards the target lerp it towards the target
+        else if (attacking)
+        {
+            transform.position = Vector3.Lerp(centre.position + armPosition, target.position, timer / attackTime);
+            if (timer > attackTime)
+            {
+                stationary = true;
+                timer = 0;
+            }
+        }
+       
+    }
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+
         if (swaying)
         {
             Swayer();
+            if (timer > intervalBetweenAttacks)
+            {
+                swaying = false;
+                attacking = true;
+                timer = 0;
+            }
+        }
+        else if(attacking)
+        {
+            Attack();
         }
     }
 }
